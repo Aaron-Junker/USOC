19a8332757d67e004409288b54f344d79701e8dd
Aaron Junker <aaron.junker@sus.schulen-stadtsh.ch>
2020-04-19 08:39:58 +0200

    push

--- site/siteelements/head.php
+++ site/siteelements/head.php
@@ -1,7 +1,6 @@
 <!-- Headfile for include -->
 <?php
 include_once "phpapi/getsettings.php";
-
 ?>
 <meta charset="utf-8">
 <base href="https://casegames.ch">
@@ -20,28 +19,16 @@ include_once "phpapi/getsettings.php";
     echo '<link rel="stylesheet" href="css.css" type="text/css" />';
   }
 ?>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-158033026-1"></script>
-<script src="https://apis.google.com/js/platform.js"></script>
 
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-158033026-1');
-</script>
 <script>
   function switchdark(c){
     document.cookie = "css=" + c;
     location.reload();
   }
 </script>
-<meta name="author" content="Aaron Junker">
-<meta name="description" content="Case Games Internetseite">
-<meta name="keywords" content="Case, Games,games,Case">
-<meta name="page-type" content="Produktinfo">
-<meta http-equiv="content-language" content="de">
-<meta name="robots" content="index, follow">
-<link rel="manifest" href="manifest.json">
+<meta name="author" content="<?php echo getSetting("site.author") ?>">
+<meta name="description" content="<?php echo getSetting("site.description") ?>">
+<meta name="keywords" content="<?php echo getSetting("site.keywords") ?>">
+<meta http-equiv="content-language" content="<?php echo getSetting("site.lang") ?>">
+<meta name="robots" content="<?php echo getSetting("site.robots") ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
