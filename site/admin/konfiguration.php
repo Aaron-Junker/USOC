f3a64c2f2f885e5b7c42b3ad40dd85e4a13ba2c7
Aaron Junker <support@casegames.ch>
2020-06-04 16:55:19 +0200

    Delete konfiguration.php

--- site/admin/konfiguration.php
+++
@@ -1,7 +0,0 @@
-<?php
-error_reporting(E_ALL);
-define ( 'MYSQL_HOST',      'localhost' );
-define ( 'MYSQL_BENUTZER',  'root' );
-define ( 'MYSQL_KENNWORT',  'password' );
-define ( 'MYSQL_DATABASE', 'USOC' );
-?>
