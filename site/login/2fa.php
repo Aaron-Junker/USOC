26f0a2fba21945fdb4f9df63f59939bee34898a5
Aaron Junker <aaron.junker@sus.schulen-stadtsh.ch>
2020-06-11 15:59:06 +0200

    Update 2fa.php

--- site/login/2fa.php
+++ site/login/2fa.php
@@ -10,15 +10,15 @@ include_once 'src/GoogleQrUrl.php';
 $g = new \Sonata\GoogleAuthenticator\GoogleAuthenticator();
 if(isset($_POST["secret"])&&isset($_POST["register"])){
   $secret = $_POST["secret"];
-  require_once ('konfiguration.php');
-  $db_link = mysqli_connect (MYSQL_HOST,MYSQL_BENUTZER,MYSQL_KENNWORT,MYSQL_DATENBANK);
+  require_once ('configuration.php');
+  $db_link = mysqli_connect (MYSQL_HOST,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DATABASE);
   $sql = "UPDATE User SET google_2fa='".$secret."' WHERE Username='".$_SESSION["User_Name"]."'";
   $db_erg = mysqli_query( $db_link, $sql );
   echo "Google Authenticator verknüpft";
 }elseif(isset($_POST["code"])&&isset($_POST["login"])){
   $code = $_POST["code"];
-  require_once ('konfiguration.php');
-  $db_link = mysqli_connect (MYSQL_HOST,MYSQL_BENUTZER,MYSQL_KENNWORT,MYSQL_DATENBANK);
+  require_once ('configuration.php');
+  $db_link = mysqli_connect (MYSQL_HOST,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DATABASE);
   $sql = "SELECT * FROM User WHERE Username='".$_SESSION['temp_User_Name']."';";
   $db_erg = mysqli_query( $db_link, $sql );
   while ($zeile = mysqli_fetch_array( $db_erg, MYSQLI_ASSOC))
@@ -31,8 +31,8 @@ if(isset($_POST["secret"])&&isset($_POST["register"])){
       }
     }
 }elseif(isset($_POST["delete"])){
-  require_once ('konfiguration.php');
-  $db_link = mysqli_connect (MYSQL_HOST,MYSQL_BENUTZER,MYSQL_KENNWORT,MYSQL_DATENBANK);
+  require_once ('configuration.php');
+  $db_link = mysqli_connect (MYSQL_HOST,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DATABASE);
   echo $_SESSION['temp_User_Name'];
   $sql = "UPDATE User SET google_2fa='' WHERE Username='".$_SESSION['User_Name']."';";
   $db_erg = mysqli_query( $db_link, $sql );
