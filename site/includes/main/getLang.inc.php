78b1468c2b55da43f699d7683262d39c614e9450
Aaron Junker <aaron.junker@sus.schulen-stadtsh.ch>
2020-06-28 08:51:37 +0200

    push

--- site/includes/main/getLang.inc.php
+++ site/includes/main/getLang.inc.php
@@ -1,12 +1,14 @@
 <?php
-  public function getLang($string){
-    //Gets a string from the translation files
-    $translate = json_decode(file_get_contents($USOC["SITE_PATH"]."lang/".getSetting("site.lang").".json"));
-    try {
-      return $translate[$string];
-    } catch (Exception $e) {
-      $translate = json_decode(file_get_contents($USOC["SITE_PATH"]."lang/en-en.json"));
-      return $translate[$string];
+  if(gettype($USOC) == "array"){
+    public function getLang($string){
+      //Gets a string from the translation files
+      $translate = json_decode(file_get_contents($USOC["SITE_PATH"]."lang/".getSetting("site.lang").".json"));
+      try {
+        return $translate[$string];
+      } catch (Exception $e) {
+        $translate = json_decode(file_get_contents($USOC["SITE_PATH"]."lang/en-en.json"));
+        return $translate[$string];
+      }
     }
   }
 ?>
