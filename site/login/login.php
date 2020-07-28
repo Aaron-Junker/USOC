518622c314ab7f736ad7427bb1d6a4b41c260b6d
Aaron Junker <aaron.junker@sus.schulen-stadtsh.ch>
2020-07-28 08:28:30 +0200

    push

--- site/login/login.php
+++ site/login/login.php
@@ -1,68 +1,63 @@
 <?php
-  include "../phpapi/getdomain.php";
-  include "../phpapi/getsettings.php";
+  require_once "../configuration.php";
+  require_once "../includes/class.inc.php";
+  $U = new U();
   session_start();
-    $blocked = False;
-    $login = False;
-    require_once ('../configuration.php');
-    $db_link = mysqli_connect (MYSQL_HOST,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DATABASE);
-    $sql = "SELECT * FROM User";
-    $db_erg = mysqli_query( $db_link, $sql );
-    if(isset($_SESSION["code"])){
-      $_POST["B"] = $_SESSION['temp_User_Name'];
-    }
-    while ($zeile = mysqli_fetch_array( $db_erg, MYSQLI_ASSOC))
-    {
-
-      if((strtolower($_POST["B"])==strtolower($zeile["Username"])||strtolower($_POST["B"])==strtolower($zeile["Mail"]))&&(@password_hash($_POST["P"],PASSWORD_DEFAULT,["salt"=>getSetting("login.salt")])==$zeile["Password"]||isset($_SESSION["code"])) ){
-
-        $login = True;
-        $user_id = $zeile["Id"];
-        $user_name = $zeile["Username"];
-        if($zeile["Type"] == 1){
-          $_SESSION["Admin"] = True;
-        }
-        if($zeile["google_2fa"] != ""){
-          $fa = True;
-        }
-        if($zeile["blocked"] == 1){
-          $login = False;
-          echo sprintf(getLang("login.locked"),getLang("login.account"));
-          $blocked = True;
-        }
+  $blocked = False;
+  $login = False;
+  $db_link = mysqli_connect (MYSQL_HOST,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DATABASE);
+  $sql = "SELECT * FROM User";
+  $db_erg = mysqli_query( $db_link, $sql );
+  if(isset($_SESSION["code"])){
+    $_POST["B"] = $_SESSION['temp_User_Name'];
+  }
+  while ($zeile = mysqli_fetch_array( $db_erg, MYSQLI_ASSOC)){
+    if((strtolower($_POST["B"])==strtolower($zeile["Username"])||strtolower($_POST["B"])==strtolower($zeile["Mail"]))&&(@password_hash($_POST["P"],PASSWORD_DEFAULT,["salt"=>getSetting("login.salt")])==$zeile["Password"]||isset($_SESSION["code"])) ){
+      $login = True;
+      $user_id = $zeile["Id"];
+      $user_name = $zeile["Username"];
+      if($zeile["Type"] == 1){
+        $_SESSION["Admin"] = True;
+      }
+      if($zeile["google_2fa"] != ""){
+        $fa = True;
+      }
+      if($zeile["blocked"] == 1){
+        $login = False;
+        echo sprintf($U->getLang("login.locked"),$U->getLang("login.account"));
+        $blocked = True;
       }
     }
-    if(getSetting("login.login_open")=="0"){
-      echo "Login geschlossen<br>";
-      $login = False;
-    }
-    if($fa && isset($_SESSION["code"])){
+  }
+  if($U->getSetting("login.login_open")=="0"){
+    echo $U->getLang("login.login_closed")."<br>";
+    $login = False;
+  }
+  if($fa && isset($_SESSION["code"])){
+    $fa = False;
+  }
+  if($fa && $login){
 
-      $fa = False;
-    }
-    if($fa && $login){
-
-      $login = 10;
-      $_SESSION["Admin"] = False;
-      $_SESSION['temp_User_ID'] = md5($user_id);
-      $_SESSION['temp_User_Name'] = $user_name;
-      $htmlcode = <<<HEREDOC
-        <form action="2fa.php" method="post">
-        <label for="code">%d</label>
-          <input name="code" />
-          <input type="submit" name="login"/>
-        </form>
-      HEREDOC;
-      echo sprintf($htmlcode, getLang("login.2fa.google_authenticator.code"))
-    }
-    if($login === True){
-
-      echo getLang("login.succeed");
-      $_SESSION['User_ID'] = md5($user_id);
-      $_SESSION['User_Name'] = $user_name;
-      header('Location: '.getDomain());
-    }elseif($login === False && $blocked ===False) {
-      echo getLang("login.fail");
-      header('Location: '.getDomain().'/login.php?ERROR=0x000000');
-    }
+    $login = 10;
+    $_SESSION["Admin"] = False;
+    $_SESSION['temp_User_ID'] = md5($user_id);
+    $_SESSION['temp_User_Name'] = $user_name;
+    $htmlcode = <<<HEREDOC
+      <form action="2fa.php" method="post">
+      <label for="code">%d</label>
+        <input name="code" />
+        <input type="submit" name="login"/>
+      </form>
+    HEREDOC;
+    echo sprintf($htmlcode, getLang("login.2fa.google_authenticator.code"))
+  }
+  if($login === True){
+    echo getLang("login.succeed");
+    $_SESSION['User_ID'] = md5($user_id);
+    $_SESSION['User_Name'] = $user_name;
+    header('Location: '.getDomain());
+  }elseif($login === False && $blocked === False) {
+    echo getLang("login.fail");
+    header('Location: '.getDomain().'/login.php?ERROR=0x000000');
+  }
 ?>
--- site/login/register.php
+++ site/login/register.php
@@ -1,4 +1,6 @@
 <?php
+  require_once "../configuration.php";
+  require_once "../includes/class.inc.php";
   $register = False;
   if(isset($_POST["U"])&&isset($_POST["M"])&&isset($_POST["P"])&&isset($_POST["PR"])&&isset($_POST["D"])&&$_POST["D"]=="on"){
     if($_POST["P"]==$_POST["PR"]){
@@ -6,8 +8,7 @@
         if(preg_match('/^[a-z0-9A-Z.]{3,15}$/',$_POST["U"])){
           if(preg_match('/^[a-z0-9A-Z.:,;]{8,25}$/',$_POST["P"])){
             $register = True;
-            require_once ('../configuration.php');
-            $db_link = mysqli_connect (MYSQL_HOST,MYSQL_BENUTZER,MYSQL_KENNWORT,MYSQL_DATENBANK);
+            $db_link = mysqli_connect (MYSQL_HOST,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DATABASE);
             $sql = "SELECT * FROM User";
             $db_erg = mysqli_query( $db_link, $sql );
             while ($zeile = mysqli_fetch_array( $db_erg, MYSQLI_ASSOC))
@@ -35,16 +36,15 @@
   }else{
     echo "Bitte alles ausfüllen";
   }
-  include "../phpapi/getsettings.php";
-  if(getSetting("login.register_open")=="0"){
-    echo "registrierung geschlossen";
+  if($U->getSetting("login.register_open")=="0"){
+    echo $U->getLang("register.closed");
     $register = False;
   }
   if($register){
     $sql = 'INSERT INTO User (Username, Mail, Password, Type) VALUES ('."'".$_POST["U"]."'".','."'".$_POST["M"]."'".','."'".password_hash($_POST["P"],PASSWORD_DEFAULT,["salt"=>getSetting("login.salt")])."'".',0);';
     if($db_erg = mysqli_query( $db_link, $sql )){
-      echo "geglückt";
-      header("Location: "+$USOC["DOMAIN"]);
+      echo $U->getLang("register.succeed");
+      header("Location: ".$USOC["DOMAIN"]);
     }else{
       echo mysqli_error($db_link);
     }
