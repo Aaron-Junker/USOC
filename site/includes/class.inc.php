26aa4d6520e819790843b261c750f885e0d8dc91
Aaron Junker <aaron.junker@sus.schulen-stadtsh.ch>
2020-09-13 10:10:31 +0200

    push

--- site/includes/class.inc.php
+++ site/includes/class.inc.php
@@ -3,11 +3,24 @@
   * This file conatins the class U. This class is used on every page.
   * @licence https://standards.casegames.ch/cgs/0003/v1.txt Case Games Open-Source licence
   */
-
+  /**
+  * This if statement checks the including of configuration.php
+  */
   if(gettype($USOC) == "array"){
+    /**
+    * This function initialise the U class in $U if it doesn't exist
+    * @version Pb2.0Bfx0
+    * @since Pb2.0Bfx0
+    */
+    function newClass(){
+      global $U;
+      $U = isset($U) ? new U() : $U;
+    }
     /**
     * This class contains all functions for USOC. When a function is needed, U includes it.
     * This class only works when configuration.php is included.
+    * Use newClass() and not this class.
+    * @see newClass()
     * @version Pb2.0Bfx0
     * @since Pb2.0Bfx0
     */
