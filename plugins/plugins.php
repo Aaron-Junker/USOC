06a142dabceb315ffb31091a9e1cda80576c51f7
Aaron Junker <aaron.junker@sus.schulen-stadtsh.ch>
2021-01-05 17:35:03 +0100

    push

--- plugins/plugins.php
+++ plugins/plugins.php
@@ -1,6 +1,7 @@
 <?php
     /**
     * This file imports all plugins.
+    * Just uncomment plugins which you want to activate
     */
-    include_once "images/index.php";
+    //include_once "images/index.php";
 ?>
